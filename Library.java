8272f644db906c7b6601314f14f8bce8a730771b
GOMATHI U <gomathiuthaya05@gmail.com>
2026-05-16 18:47:09 +0530

    Implement library management methods

--- Library.java
+++ Library.java
@@ -1,1 +1,43 @@
+import java.util.ArrayList;
 
+public class Library {
+    ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added successfully!");
+    }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+            return;
+        }
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public void issueBook(int id) {
+        for (Book book : books) {
+            if (book.getBookId() == id && !book.isIssued()) {
+                book.issueBook();
+                System.out.println("Book issued successfully!");
+                return;
+            }
+        }
+        System.out.println("Book unavailable or not found.");
+    }
+
+    public void returnBook(int id) {
+        for (Book book : books) {
+            if (book.getBookId() == id && book.isIssued()) {
+                book.returnBook();
+                System.out.println("Book returned successfully!");
+                return;
+            }
+        }
+        System.out.println("Book not found or already available.");
+    }
+}
